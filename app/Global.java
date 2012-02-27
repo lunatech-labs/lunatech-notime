e3c13fd5ea1f6ff27e6505449ca5c673918d89c3
Leonard Punt <leonardpunt@gmail.com>
2012-02-27 15:16:18 +0100

    Added initial data

---
+++ app/Global.java
@@ -0,0 +1,47 @@
+import play.*;
+import play.libs.*;
+
+import java.util.*;
+
+import com.avaje.ebean.*;
+
+import models.*;
+
+public class Global extends GlobalSettings {
+    
+    public void onStart(Application app) {
+        InitialData.insert(app);
+    }
+    
+    static class InitialData {
+        
+        public static void insert(Application app) {
+            if(Ebean.find(User.class).findRowCount() == 0) {
+                
+                Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
+
+                // Insert users first
+                Ebean.save(all.get("users"));
+
+                // Insert customers
+                Ebean.save(all.get("customers"));
+                for(Object user: all.get("customers")) {
+                    // Insert the user/customer relation
+                    Ebean.saveManyToManyAssociations(user, "customerManagers");
+                }
+                
+                // Insert projects
+                Ebean.save(all.get("projects"));
+                for(Object project: all.get("projects")) {
+                    // Insert the project/customer relation
+                    Ebean.saveAssociation(project, "customer");
+                    // Insert the project/user relation
+                    Ebean.saveAssociation(project, "projectManager");
+                }
+                
+            }
+        }
+        
+    }
+    
+}
