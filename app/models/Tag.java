3e03de6c2725e3fe78e282dc9a4778c336ec63a2
Leonard Punt <leonardpunt@gmail.com>
2012-03-08 17:02:46 +0100

    TIME-55, fixed tags already in TIME-1, the classification is fixed by displaying all the used tags.

--- app/models/Tag.java
+++ app/models/Tag.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,20 +16,34 @@ import play.db.jpa.JPA;
 public class Tag {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tag_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tag_seq")
 	public Long id;
-	
+
 	@Required
 	public String tag;
-	
+
 	public static void create(Tag tag) {
 		JPA.em().persist(tag);
 	}
-	
+
 	public static Tag create(String tagString) {
 		Tag tag = new Tag();
 		tag.tag = tagString.trim();
 		JPA.em().persist(tag);
 		return tag;
+	}
+
+	public static String tagsString() {
+		List<String> tags = JPA.em().createQuery("select tag from Tag").getResultList();
+		String tagsString = new String();
+		for(int i = 0; i < tags.size(); i++) {
+			if(tags.size()-1 == i) {
+				tagsString += tags.get(i) + ".";
+			}
+			else {
+				tagsString += tags.get(i) + ", ";
+			}		
+		}
+		return tagsString;
 	}
 }
