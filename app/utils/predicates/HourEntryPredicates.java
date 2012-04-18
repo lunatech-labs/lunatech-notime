186ce3e5d2355e74bc45d2b1116cb375d3e8e976
Leonard Punt <leonardpunt@gmail.com>
2012-04-18 14:36:56 +0200

    simplified report assignmentTotalsPerUser

--- app/utils/predicates/HourEntryPredicates.java
+++ app/utils/predicates/HourEntryPredicates.java
@@ -78,46 +78,4 @@ public class HourEntryPredicates {
 		}
 	}
 
-	/**
-	 * A predicate that checks if the user of a {@link HourEntry} is equal to
-	 * the provided user and if the {@link HourEntry}'s date is between the
-	 * provided dates
-	 * 
-	 * @param user
-	 *            User to which the {@link HourEntry}'s user must be equal to
-	 * @param beginDate
-	 *            The date from which entries are filtered
-	 * @param endDate
-	 *            The date till which entries are filtered
-	 * @return A {@link Predicate}
-	 */
-	public static Predicate<HourEntry> equalUserAndBetweenDates(
-			final User user, final LocalDate beginDate, final LocalDate endDate) {
-		return new EqualUserAndBetweenDates(user, beginDate, endDate);
-	}
-
-	/**
-	 * A predicate that checks if the user of a {@link HourEntry} is equal to
-	 * the provided user and if the {@link HourEntry}'s date is between the
-	 * provided dates
-	 */
-	private static class EqualUserAndBetweenDates implements
-			Predicate<HourEntry> {
-		private final User user;
-		private final LocalDate beginDate;
-		private final LocalDate endDate;
-
-		private EqualUserAndBetweenDates(final User user,
-				final LocalDate beginDate, final LocalDate endDate) {
-			this.user = user;
-			this.beginDate = beginDate;
-			this.endDate = endDate;
-		}
-
-		public boolean apply(final HourEntry entry) {
-			return entry.assignment.user == user
-					&& DateUtil.between(entry.date, beginDate, endDate);
-		}
-	}
-
 }
